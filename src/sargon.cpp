71d79168064874cacec541f7babdd60a97f7167a
Bill Forster <billforsternz@gmail.com>
2019-11-26 18:37:39 +1300

    Refactor and tidy up interface to Sargon and tests, getting rid of some junk that no longer applies now that Sargon is smoothly calculating moves

--- src/sargon.cpp
+++ src/sargon.cpp
@@ -25,18 +25,23 @@
 #include "sargon-asm-interface.h"
 #include "translate.h"
 
-unsigned char *sargon_format_position_create( const char *fen );
-std::string sargon_format_position_read( thc::ChessPosition &cp, const char *msg=0 );
-void show_board_layout( const unsigned char *sargon_board, std::string msg );
-void peek();
-void diagnostics();
-void pokeb( int offset, unsigned char b );
-unsigned char peekb(int offset);
-void sargon_tests();
+// Read chess position from Sargon
+void sargon_position_export( thc::ChessPosition &cp );
 
-// For peeking
-static unsigned char *sargon_mem_base;
-const int BOARD_SIZE    = 120;
+// Write chess position into Sargon
+void sargon_position_import( const thc::ChessPosition &cp );
+
+// Peek and poke at Sargon
+const unsigned char *peek(int offset);
+unsigned char peekb(int offset);
+unsigned int peekw(int offset);
+unsigned char *poke(int offset);
+void pokeb( int offset, unsigned char b );
+
+// Misc diagnostics
+void dbg_ptrs();
+void diagnostics();
+void sargon_tests();
 
 std::map<std::string,unsigned long> func_counts;
 void count_functions( const char *msg )
@@ -82,7 +87,7 @@ extern "C" {
         count_functions(msg);
         //printf( "Sargon diagnostics callback %s\n", msg );
         //if( msg[0] == '*' )
-        //    peek();
+        //    dbg_ptrs();
         //else
         //    diagnostics();
     }
@@ -99,7 +104,7 @@ void sargon_tests()
 {
 /*
 
-Test position 2
+Test position 1
 
 FEN r2n2k1/5ppp/b5q1/1P3N2/8/8/3Q1PPP/3R2K1 w - - 0 1
 
@@ -115,40 +120,28 @@ b.....q.
 White to play has three forcing wins, requiring
 increasing depth for increasing reward;
 
+In theory:
 Immediate capture of bishop b5xa6 (wins a piece) [1 ply]
 Royal fork Nf5-e7 (wins queen) [3 ply]
 Back rank mate Qd2xd8+ [4 ply maybe]
+In practice
+Mate found with PLYMAX 3 or greater
+Royal fork found with PLYMAX 1 or 2
 
 To make the back rank mate a little harder, move the
 Pb5 and ba6 to Pb3 and ba4 so bishop can postpone mate
-by one move.
-
-ff ff ff ff ff ff ff ff ff ff
-ff ff ff ff ff ff ff ff ff ff
-ff 00 00 00 0c 00 00 0e 00 ff
-ff 00 00 00 0d 00 01 01 01 ff
-ff 00 00 00 00 00 00 00 00 ff
-ff 00 00 00 00 00 00 00 00 ff
-ff 00 09 00 00 00 0a 00 00 ff
-ff 8b 00 00 00 00 00 8d 00 ff
-ff 00 00 00 00 00 81 81 81 ff
-ff 8c 00 00 8a 00 00 8e 00 ff
-ff ff ff ff ff ff ff ff ff ff
-ff ff ff ff ff ff ff ff ff ff
-
+by one move. As expected mate now requires greater
+depth, so royal fork for PLYMAX 1-4, mate if PLYMAX 5
 
 */
 
     // CTWBFK = "Chess Tactics Workbook For Kids'
     const char *pos1 = "r2n2k1/5ppp/b5q1/1P3N2/8/8/3Q1PPP/3R2K1 w - - 0 1";             // Test position #1 above
-    const char *pos1a = "r2n2k1/5pp1/b5qp/1P3N2/8/8/3Q1PPP/3R2K1 w - - 0 1";            // Slight mod, h7-h6 so it's not mate, yet Sargon still plays Qd2xNd8 with ply max = 1
-                                                                                        //  and an easy piece capture on a6 available. Why?
-    const char *pos1b = "r2n2k1/5pp1/b5qp/1P6/8/5N2/3Q1PPP/3R2K1 w - - 0 1";            //  Still does it even if we make N safe on f3 - at max ply 1 and 2
-    const char *pos1c = "r2n4/5ppk/b5qp/1P6/8/5N2/3Q1PPP/3R2K1 w - - 0 1";              //  Even with king on h7 (so no check), still does it at max ply 1 (but not 2)
-    const char *pos1d = "r2n4/5ppk/b5qp/8/1P6/5N2/3Q1PPP/2R3K1 w - - 0 1";              //  With king on h7 (so no check), pawn back to b4 (so no capture in reserve)
-                                                                                        //   and rook on c1 (so giving up Q for N - not Q for N+R) Sargon plays Qxd8
-                                                                                        //   at max ply 1 - so maybe it just thinks it is winning N and doesn't account
-                                                                                        //   for defending rook at all.
+    const char *pos1b = "r2n2k1/5ppp/6q1/5N2/b7/1P6/3Q1PPP/3R2K1 w - - 0 1";            // Modified version as discussed above
+                                                                                        // Used to have a lot of other mods as we
+                                                                                        // tried to figure out what was going wrong before
+                                                                                        // we got some things sorted out (notably need to
+                                                                                        // call ROYALT before CPTRMV!)
     const char *pos2 = "2r1nrk1/5pbp/1p2p1p1/8/p2B4/PqNR2P1/1P3P1P/1Q1R2K1 w - - 0 1";  // CTWBFK Pos 30, page 41 - solution Nc3-d5
     const char *pos3 = "5k2/3KR3/4B3/8/3P4/8/8/6q1 w - - 0 1";                          // CTWBFK Pos 34, page 62 - solution Re7-f7+
     const char *pos4 = "3r2k1/1pq2ppp/pb1pp1b1/8/3B4/2N5/PPP1QPPP/4R1K1 w - - 0 1";     // CTWBFK Pos 7, page 102 - solution Nc3-d5
@@ -165,28 +158,26 @@ ff ff ff ff ff ff ff ff ff ff
                                                                     //  add 2 to convert PLYMAX to calculation depth
     const char *pos8 = "3k4/8/8/7P/8/8/1p6/1K6 w - - 0 1";  // Pawn outside the square needs PLYMAX=5 to solve
     const char *pos9 = "2k5/8/8/8/7P/8/1p6/1K6 w - - 0 1";  // Pawn one further step back needs, as expected PLYMAX=7 to solve
-    unsigned char *test_position = sargon_format_position_create( pos6 );
-
-    sargon_mem_base = &sargon_base_address;
-    unsigned char *sargon_board = sargon_mem_base + BOARDA;
-    const unsigned char *sargon_move_made = sargon_mem_base + MVEMSG;
+    thc::ChessPosition cp;
+    cp.Forsyth(pos9);
     pokeb(COLOR,0);
     pokeb(KOLOR,0);
-    pokeb(PLYMAX,7);
+    pokeb(PLYMAX,5);
     sargon(api_INITBD);
-    thc::ChessPosition cp;
-    std::string s = sargon_format_position_read( cp, "Position after board initialised" );
-    printf( "%s\n", s.c_str() );
-    memcpy( sargon_board, test_position, BOARD_SIZE );
+    sargon_position_import(cp);
     sargon(api_ROYALT);
-    s = sargon_format_position_read( cp, "Position after test position set" );
+    sargon_position_export(cp);
+    std::string s = cp.ToDebugStr( "Position after test position set" );
     printf( "%s\n", s.c_str() );
     pokeb(MOVENO,3);    // Move number is 1 at at start, add 2 to avoid book move
     sargon(api_CPTRMV);
-    s = sargon_format_position_read( cp, "Position after computer move made" );
-    printf( "%s", s.c_str() );
-    const unsigned char *q = sargon_move_made;
-    printf( "\nMove made is: %c%c-%c%c\n", q[0],q[1],q[2],q[3] );
+    sargon_position_export(cp);
+    s = cp.ToDebugStr( "Position after computer move made" );
+    printf( "%s\n", s.c_str() );
+    char buf[5];
+    memcpy( buf, peek(MVEMSG), 4 );
+    buf[4] = '\0';
+    printf( "\nMove made is: %s\n", buf );
     int offset = MLEND;
     while( offset > 0 )
     {
@@ -200,99 +191,11 @@ ff ff ff ff ff ff ff ff ff ff
     }
 }
 
-unsigned char *sargon_format_position_create( const char *fen )
-{
-    static unsigned char board_position[120] =
-    {
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0x04, 0x02, 0x03, 0x05, 0x06, 0x03, 0x02, 0x04, 0xff,  // <-- White back row, a1 at left end, h1 at right end
-        0xff, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0xff,  // <-- White pawns
-        0xff, 0xa3, 0xb3, 0xc3, 0xd3, 0xe3, 0xf3, 0x00, 0x00, 0xff,  // <-- note 'a3' etc just documents square convention
-        0xff, 0xa4, 0xb4, 0xc4, 0xd4, 0xe4, 0xf4, 0x00, 0x00, 0xff,  // <-- but g and h files are 0x00 = empty
-        0xff, 0xa5, 0xb5, 0xc5, 0xd5, 0xe5, 0xf5, 0x00, 0x00, 0xff,
-        0xff, 0xa6, 0xb6, 0xc6, 0xd6, 0xe6, 0xf6, 0x00, 0x00, 0xff,
-        0xff, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xff,  // <-- Black pawns
-        0xff, 0x84, 0x82, 0x83, 0x85, 0x86, 0x83, 0x82, 0x84, 0xff,  // <-- Black back row
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-    };
-
-    thc::ChessPosition cr;
-    cr.Forsyth( fen );
-    memset( board_position, 0xff, sizeof(board_position) );
-    unsigned char *dst_base = board_position + 28;  // square h1
-    char *src_base = &cr.squares[thc::h1];
-    for( int i=0; i<8; i++ )
-    {
-        unsigned char *dst = dst_base + i*10;
-        char *src = src_base - i*8;
-        for( int j=0; j<8; j++ )
-        {
-            char c = *src--;
-            unsigned char b = 0;
-            switch( c )
-            {
-                case 'p':   b = 0x81;   break;
-                case 'n':   b = 0x82;   break;
-                case 'b':   b = 0x83;   break;
-                case 'r':   b = 0x84;   break;
-                case 'q':   b = 0x85;   break;
-                case 'k':   b = 0x86;   break;
-                case 'P':   b = 0x01;   break;
-                case 'N':   b = 0x02;   break;
-                case 'B':   b = 0x03;   break;
-                case 'R':   b = 0x04;   break;
-                case 'Q':   b = 0x05;   break;
-                case 'K':   b = 0x06;   break;
-            }
-            bool moved=true;
-            if( i==0 )
-            {
-                if( j==0 && c=='R' && cr.wking_allowed() )
-                    moved = false; 
-                if( (j==1||j==6) && c=='N' )
-                    moved = false; 
-                if( (j==2||j==5) && c=='B' )
-                    moved = false; 
-                if( j==3 && c=='K' && (cr.wking_allowed()||cr.wqueen_allowed()) )
-                    moved = false; 
-                if( j==4 && c=='Q' )
-                    moved = false; 
-                if( j==7 && c=='R' && cr.wqueen_allowed() )
-                    moved = false; 
-            }
-            else if( i==1 && c=='P' )
-                moved = false; 
-            else if( i==6 && c=='p' )
-                moved = false; 
-            else if( i==7 )
-            {
-                if( j==0 && c=='r' && cr.bking_allowed() )
-                    moved = false; 
-                if( (j==1||j==6) && c=='n' )
-                    moved = false; 
-                if( (j==2||j==5) && c=='b' )
-                    moved = false; 
-                if( j==3 && c=='k' && (cr.bking_allowed()||cr.bqueen_allowed()) )
-                    moved = false; 
-                if( j==4 && c=='q' )
-                    moved = false; 
-                if( j==7 && c=='r' && cr.bqueen_allowed() )
-                    moved = false; 
-            }
-            if( moved && b!=0 )
-                b += 8;
-            *dst-- = b;
-        }
-    }
-    return board_position;
-}
-
-std::string sargon_format_position_read( thc::ChessPosition &cp, const char *msg )
+// Read chess position from Sargon
+void sargon_position_export( thc::ChessPosition &cp )
 {
     cp.Init();
-    const unsigned char *sargon_board = sargon_mem_base + BOARDA;
+    const unsigned char *sargon_board = peek(BOARDA);
     const unsigned char *src_base = sargon_board + 28;  // square h1
     char *dst_base = &cp.squares[thc::h1];
     for( int i=0; i<8; i++ )
@@ -322,25 +225,113 @@ std::string sargon_format_position_read( thc::ChessPosition &cp, const char *msg
             *dst-- = c;
         }
     }
-    cp.white = (*(sargon_mem_base+COLOR) == 0x00);
-    std::string s = cp.ToDebugStr(msg?msg:"");
-    return s;
+    cp.white = (peekb(COLOR) == 0x00);
 }
 
-void show_board_layout( const unsigned char *sargon_board, std::string msg )
+// Write chess position into Sargon
+void sargon_position_import( const thc::ChessPosition &cp )
 {
-    printf( "%s\n", msg.c_str() );
-    const unsigned char *p = sargon_board;
-    for( int i=0; i<12; i++ )
+    static unsigned char board_position[120] =
     {
-        for( int j=0; j<10; j++ )
-            printf( "%02x%c", *p++, j+1<10?' ':'\n' );
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0x04, 0x02, 0x03, 0x05, 0x06, 0x03, 0x02, 0x04, 0xff,  // <-- White back row, a1 at left end, h1 at right end
+        0xff, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0xff,  // <-- White pawns
+        0xff, 0xa3, 0xb3, 0xc3, 0xd3, 0xe3, 0xf3, 0x00, 0x00, 0xff,  // <-- note 'a3' etc just documents square convention
+        0xff, 0xa4, 0xb4, 0xc4, 0xd4, 0xe4, 0xf4, 0x00, 0x00, 0xff,  // <-- but g and h files are 0x00 = empty
+        0xff, 0xa5, 0xb5, 0xc5, 0xd5, 0xe5, 0xf5, 0x00, 0x00, 0xff,
+        0xff, 0xa6, 0xb6, 0xc6, 0xd6, 0xe6, 0xf6, 0x00, 0x00, 0xff,
+        0xff, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0x81, 0xff,  // <-- Black pawns
+        0xff, 0x84, 0x82, 0x83, 0x85, 0x86, 0x83, 0x82, 0x84, 0xff,  // <-- Black back row
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+    };
+
+    memset( board_position, 0xff, sizeof(board_position) );
+    unsigned char *dst_base = board_position + 28;  // square h1
+    const char *src_base = &cp.squares[thc::h1];
+    for( int i=0; i<8; i++ )
+    {
+        unsigned char *dst = dst_base + i*10;
+        const char *src = src_base - i*8;
+        for( int j=0; j<8; j++ )
+        {
+            char c = *src--;
+            unsigned char b = 0;
+            switch( c )
+            {
+                case 'p':   b = 0x81;   break;
+                case 'n':   b = 0x82;   break;
+                case 'b':   b = 0x83;   break;
+                case 'r':   b = 0x84;   break;
+                case 'q':   b = 0x85;   break;
+                case 'k':   b = 0x86;   break;
+                case 'P':   b = 0x01;   break;
+                case 'N':   b = 0x02;   break;
+                case 'B':   b = 0x03;   break;
+                case 'R':   b = 0x04;   break;
+                case 'Q':   b = 0x05;   break;
+                case 'K':   b = 0x06;   break;
+            }
+            bool moved=true;
+            if( i==0 )
+            {
+                if( j==0 && c=='R' && cp.wking_allowed() )
+                    moved = false; 
+                if( (j==1||j==6) && c=='N' )
+                    moved = false; 
+                if( (j==2||j==5) && c=='B' )
+                    moved = false; 
+                if( j==3 && c=='K' && (cp.wking_allowed()||cp.wqueen_allowed()) )
+                    moved = false; 
+                if( j==4 && c=='Q' )
+                    moved = false; 
+                if( j==7 && c=='R' && cp.wqueen_allowed() )
+                    moved = false; 
+            }
+            else if( i==1 && c=='P' )
+                moved = false; 
+            else if( i==6 && c=='p' )
+                moved = false; 
+            else if( i==7 )
+            {
+                if( j==0 && c=='r' && cp.bking_allowed() )
+                    moved = false; 
+                if( (j==1||j==6) && c=='n' )
+                    moved = false; 
+                if( (j==2||j==5) && c=='b' )
+                    moved = false; 
+                if( j==3 && c=='k' && (cp.bking_allowed()||cp.bqueen_allowed()) )
+                    moved = false; 
+                if( j==4 && c=='q' )
+                    moved = false; 
+                if( j==7 && c=='r' && cp.bqueen_allowed() )
+                    moved = false; 
+            }
+            if( moved && b!=0 )
+                b += 8;
+            *dst-- = b;
+        }
     }
+    memcpy( poke(BOARDA), board_position, sizeof(board_position) );
+}
+
+const unsigned char *peek(int offset)
+{
+    unsigned char *sargon_mem_base = &sargon_base_address;
+    unsigned char *addr = sargon_mem_base + offset;
+    return addr;
+}
+
+unsigned char peekb(int offset)
+{
+    const unsigned char *addr = peek(offset);
+    return *addr;
 }
 
 unsigned int peekw(int offset)
 {
-    const unsigned char *addr = sargon_mem_base + offset;
+    const unsigned char *addr = peek(offset);
     const unsigned char lo = *addr++;
     const unsigned char hi = *addr++;
     unsigned int ret = hi;
@@ -349,20 +340,20 @@ unsigned int peekw(int offset)
     return ret;
 }
 
-unsigned char peekb(int offset)
+unsigned char *poke(int offset)
 {
-    const unsigned char *addr = sargon_mem_base + offset;
-    unsigned int ret = *addr;
-    return ret;
+    unsigned char *sargon_mem_base = &sargon_base_address;
+    unsigned char *addr = sargon_mem_base + offset;
+    return addr;
 }
 
 void pokeb( int offset, unsigned char b )
 {
-    unsigned char *addr = sargon_mem_base + offset;
+    unsigned char *addr = poke(offset);
     *addr = b;
 }
 
-void peek()
+void dbg_ptrs()
 {
     printf( "MLPTRI = %04x\n", peekw(MLPTRI) );
     printf( "MLPTRJ = %04x\n", peekw(MLPTRJ) );
@@ -387,7 +378,7 @@ std::string algebraic( unsigned int sq )
 
 void diagnostics()
 {
-    peek();
+    dbg_ptrs();
     unsigned int p = peekw(MLPTRI);
     printf( "MLPTRI chain\n" );
     for( int i=0; i<50 && (p); i++ )
@@ -400,7 +391,6 @@ void diagnostics()
         printf( "link=0x%04x, from=%s, to=%s flags=0x%02x value=%u\n", p, algebraic(from).c_str(), algebraic(to).c_str(), flags, value );
     }
 }
-
 
 
 /*
