52b0a3ed8a44058cd5d4a25cfebd6a6c9c623308
Bill Forster <billforsternz@gmail.com>
2019-12-14 17:08:24 +1300

    Move some semi-boilerplate code out of client code into thc library, at least temporarily

--- src/thc.cpp
+++ src/thc.cpp
@@ -14122,4 +14122,32 @@ lte to_mask[] =
 
 }
 
+/*
+ *  For now move this here to avoid constantly needing to provide it client code
+ */
 
+#ifndef KILL_DEBUG_COMPLETELY
+int core_printf( const char *fmt, ... )
+{
+    int ret=0;
+	va_list args;
+	va_start( args, fmt );
+    char buf[1000];
+    char *p = buf;
+    vsnprintf( p, sizeof(buf)-2-(p-buf), fmt, args ); 
+    fputs(buf,stdout);
+    va_end(args);
+    return ret;
+}
+#endif
+
+void ReportOnProgress
+(
+    bool    init,
+    int     multipv,
+    std::vector<thc::Move> &pv,
+    int     score_cp,
+    int     depth
+)
+{
+}
