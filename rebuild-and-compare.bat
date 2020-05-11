5192e8e6b09418c3f842c6ca2e5b3b0adfbfc0b8
Bill Forster <billforsternz@gmail.com>
2020-05-11 18:17:16 +1200

    Add check of stages\sargon-x86.asm and stages\sargon-asm-interface.h against src versions

--- rebuild-and-compare.bat
+++ rebuild-and-compare.bat
@@ -5,6 +5,8 @@ Release\convert-z80-to-x86.exe -relax stages\sargon-z80-and-x86.asm temp-sargon-
 Release\convert-z80-to-x86.exe -z80_only stages\sargon-z80-and-x86.asm temp-sargon-z80.asm temp-interface.h temp-report.txt
 fc stages\sargon-x86.asm temp-sargon-x86.asm
 fc stages\sargon-asm-interface.h temp-sargon-asm-interface.h
+fc stages\sargon-x86.asm src\sargon-x86.asm
+fc stages\sargon-asm-interface.h src\sargon-asm-interface.h
 fc stages\sargon-z80.asm temp-sargon-z80.asm
 REM fc stages\sargon-z80.asm stages\sargon4.asm
 REM Unfortunately, sargon4.asm and sargon-z80.asm are a bit too different for fc (use winmerge perhaps), other fc commands should reveal no differences
